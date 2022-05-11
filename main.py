2f8f73ef7b75eb6905c52d5018711681d4753277
Larborator <466902955@qq.com>
2022-05-12 00:47:33 +0800

    feat: 优化main.py模板格式

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 """
+https://leetcode.cn/problems/
 
 author: labuladuo
 date: 2022/4/14
 """
 
 
-def myFunction():
-    pass
+class Solution:
+    def myFunction(self, args):
+        print(args)
+        pass
 
 
 if __name__ == '__main__':
-    myFunction()
+    s = Solution()
+    s.myFunction("hello")
