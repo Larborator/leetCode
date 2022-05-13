75c9c3b75bf92cfafa0d6a99b30378762dc97ea1
labuladuo <labuladuo@douyu.tv>
2022-05-13 20:17:06 +0800

    note: 正则

--- 8.py
+++ 8.py
@@ -1,13 +1,23 @@
 """
+https://leetcode.cn/problems/string-to-integer-atoi/
 
 author: labuladuo
 date: 2022/4/14
 """
+import re
 
 
-def myFunction():
-    pass
+class Solution:
+    def myFunction(self, str: str) -> int:
+        INT_MAX = 2147483647
+        INT_MIN = -2147483648
+        str = str.lstrip()
+        num_re = re.compile(r'^[\+\-]?\d+')
+        num = num_re.findall(str)
+        num = int(*num)
+        return max(min(num, INT_MAX), INT_MIN)
 
 
 if __name__ == '__main__':
-    myFunction()
+    s = Solution()
+    print(s.myFunction("0123"))
